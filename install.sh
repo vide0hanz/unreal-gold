ec56fb77a7006c0fab24411b1ba740dfc6a755e9
vide0hanz <nacho_dog@me.com>
2021-08-15 14:53:13 -0500

    Updated and simplified repo

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+game="unreal_gold"
+exe="setup_unreal_gold_2.0.0.6.exe"
+patch="unreal-linux.tar.bz2"
+
+lgogdownloader --download --game "$game"
+mv unreal_gold/"$exe" .
+innoextract --extract --exclude-temp "$exe"
+mv app/* .
+tar vvxf ${patch}
+
+#cleanup
+rm -rf unreal_gold app tmp && echo "installation comple! Remember to edit the unreal-linux script accordingly, and place it in your PATH"
